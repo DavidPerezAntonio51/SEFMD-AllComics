cc0a958926af7e8f80860ae3c265370be9afdbca
GraysonDN <luis.rostro.valdez@gmail.com>
2022-05-05 18:13:20 -0500

    Merge branch 'main' of https://github.com/DavidPerezAntonio51/SEFMD-AllComics

--- allcomics-front/src/Paginas/Home.jsx
+++ allcomics-front/src/Paginas/Home.jsx
@@ -1,48 +1,48 @@
 
-import { Container, } from "react-bootstrap";
-
+import { Carousel, Container, } from "react-bootstrap";
+import Carrusel_Layout1 from "../Assests/Carrusel_Layout1.png"
 function Home(props) {
-    return ( 
-        <Container>
-                    <Carousel>
-  <Carousel.Item>
-    <img
-      className="d-block w-100"
-      src="holder.js/800x400?text=First slide&bg=373940"
-      alt="Wellcome to ALLComics"
-    />
-    <Carousel.Caption>
-      <h3>Pagina dedicada a venta de comics</h3>
-      <p>Aqui podras encontrar los mejores comics de varias tiendas en el pais</p>
-    </Carousel.Caption>
-  </Carousel.Item>
-  <Carousel.Item>
-    <img
-      className="d-block w-100"
-      src="holder.js/800x400?text=Second slide&bg=282c34"
-      alt="Second slide"
-    />
+  return (
+    <Container>
+      <Carousel>
+        <Carousel.Item>
+          <img
+            className="d-block w-100"
+            src={Carrusel_Layout1}
+            alt="Wellcome to ALLComics"
+          />
+          <Carousel.Caption>
+          <h3>Tienda de comics no autorizada</h3>
+          <p>Estaremos subiendo contenido proximamente.</p>
+          </Carousel.Caption>
+        </Carousel.Item>
+        <Carousel.Item>
+          <img
+            className="d-block w-100"
+            src="holder.js/800x400?text=Second slide&bg=282c34"
+            alt="Second slide"
+          />
 
-    <Carousel.Caption>
-      <h3>Coming Soon...</h3>
-      <p>Estaremos subiendo contenido proximamente.</p>
-    </Carousel.Caption>
-  </Carousel.Item>
-  <Carousel.Item>
-    <img
-      className="d-block w-100"
-      src="holder.js/800x400?text=Third slide&bg=20232a"
-      alt="Third slide"
-    />
+          <Carousel.Caption>
+            <h3>Coming Soon...</h3>
+            <p>Estaremos subiendo contenido proximamente.</p>
+          </Carousel.Caption>
+        </Carousel.Item>
+        <Carousel.Item>
+          <img
+            className="d-block w-100"
+            src="holder.js/800x400?text=Third slide&bg=20232a"
+            alt="Third slide"
+          />
 
-    <Carousel.Caption>
-      <h3>Coming Soon...</h3>
-      <p>Estaremos subiendo contenido proximamente.</p>
-    </Carousel.Caption>
-  </Carousel.Item>
-</Carousel>
-        </Container>
-    );
+          <Carousel.Caption>
+            <h3>Coming Soon...</h3>
+            <p>Estaremos subiendo contenido proximamente.</p>
+          </Carousel.Caption>
+        </Carousel.Item>
+      </Carousel>
+    </Container>
+  );
 }
 
 export default Home;
