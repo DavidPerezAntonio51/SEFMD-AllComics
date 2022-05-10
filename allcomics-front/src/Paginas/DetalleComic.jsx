a26eb52b54ffe09616f4cc913083a48ed8cbdac2
GraysonDN <luis.rostro.valdez@gmail.com>
2022-05-10 13:21:46 -0500

    MENU DE COMPRA

--- allcomics-front/src/Paginas/DetalleComic.jsx
+++ allcomics-front/src/Paginas/DetalleComic.jsx
@@ -7,7 +7,7 @@ function DetalleComic() {
             <Row>
                 <Col>
                     <img src={SpiderMan_2099}
-                        className="rounded float-left"
+                       className="padding"
                     />
                 </Col>
                 <Col>
@@ -19,27 +19,42 @@ function DetalleComic() {
                             Serie
                         </Col>
                         <Col>
-                            Precio
+                            Editorial
                         </Col>
                     </Row>
                     <p className="p-2 mb-2 bg-dark text-white text-center">Descripción del comic</p>
                     <p className="text-muted text-center">What is Lorem Ipsum?
                         Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.</p>
-                    <p className="p-2 mb-2 bg-dark text-white text-center">Editorial</p>
-                    <p className="text-muted text-center">Panini Comics</p>
+                    <p className="p-2 mb-2 bg-dark text-white text-center">Portada</p>
+                    <p className="text-muted text-center">Regular</p>
                     <p className="p-2 mb-2 bg-dark text-white text-center">Disponibilidad</p>
                     <Col>
                         <Table striped bordered hover>
                             <thead>
                                 <tr>
                                     <th colSpan={2}>Tienda</th>
-                                    <th>Cantidad</th>
+                                    <th>Pz Disponibles:</th>
                                     <th>Precio</th>
                                 </tr>
+                               
                             </thead>
                             <tbody>
 
-
+                            <tr>
+                                    <th colSpan={2}>Comics Mexico</th>
+                                    <th> 5 </th>
+                                    <th>$129.00</th>
+                                </tr>
+                                <tr>
+                                    <th colSpan={2}>Xanddar Comics</th>
+                                    <th> 2 </th>
+                                    <th>$169.00</th>
+                                </tr>
+                                <tr>
+                                    <th colSpan={2}>Xibalbá Store</th>
+                                    <th> 2 </th>
+                                    <th>$130.00</th>
+                                </tr>
                             </tbody>
 
                         </Table>
@@ -47,13 +62,11 @@ function DetalleComic() {
                     <Col>
                         <Form.Select aria-label="Default select example">
                             <option>Seleccione una opcion de compra</option>
-                            <option value="1">Comics Mexico        /       5pz Disponibles        /                     $129.00</option>
-                            <option value="2">Xanddar Comics       /       2pz Disponibles        /                     $169.00</option>
-                            <option value="3">Xibalbá Store        /       2pz Disponibles        /                     $130.00</option>
+                            <option value="1">Comics Mexico </option>
+                            <option value="2">Xanddar Comics</option>
+                            <option value="3">Xibalbá Store </option>
                         </Form.Select>
                     </Col>
-                    <p className="p-2 mb-2 bg-dark text-white text-center">Agregar al carrito de compra</p>
-
                     <Stack direction="horizontal" gap={3}>
                         <Form.Control className="me-auto" placeholder="seleccione la cantidad a agregar" type="number" />
                         <Button variant="secondary">Añadir al carrito</Button>
