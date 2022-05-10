d7b3585833e300bad8316f1343da4defbda6b058
GraysonDN <luis.rostro.valdez@gmail.com>
2022-05-10 13:22:45 -0500

    cambios al carrusel

--- allcomics-front/src/Paginas/Home.jsx
+++ allcomics-front/src/Paginas/Home.jsx
@@ -1,8 +1,10 @@
+
 import { Carousel, Col, Row, Button } from "react-bootstrap";
 import { Container, } from "react-bootstrap";
 import Carrusel_Layout1 from "../Assests/Carrusel_Layout1.png";
+import Carrusel_Layout2 from "../Assests/Carrusel_Layout2.png"
+import Carrusel_Layout3 from "../Assests/Carrusel_Layout3.png"
 import Layout1 from "../Assests/Layout1.png"
-
 function Home(props) {
   return (
 
@@ -18,31 +20,26 @@ function Home(props) {
             <Carousel.Item>
               <img
                 className="d-block w-100"
-
                 src={Carrusel_Layout1}
                 alt="Wellcome to ALLComics"
               />
               <Carousel.Caption>
-
                 <p>Tienda de comics no autorizada</p>
               </Carousel.Caption>
             </Carousel.Item>
+            
             <Carousel.Item>
               <img
                 className="d-block 	w-100"
-                src={Carrusel_Layout1}
+                src={Carrusel_Layout2}
                 alt="Second slide"
               />
 
-              <Carousel.Caption>
-                <h3>Coming Soon...</h3>
-                <p>Estaremos subiendo contenido proximamente.</p>
-              </Carousel.Caption>
             </Carousel.Item>
             <Carousel.Item>
               <img
                 className="d-block w-100"
-                src={Carrusel_Layout1}
+                src={Carrusel_Layout3}
                 alt="Third slide"
               />
 
@@ -65,6 +62,9 @@ function Home(props) {
         </h1>
       </Row>
       <Row></Row>
+
+      <Container>
+      </Container>
     </>
   );
 }
