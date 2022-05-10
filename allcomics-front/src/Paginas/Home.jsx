113c63bb826cac42305e35b0d01eb23ecaf2131b
GraysonDN <luis.rostro.valdez@gmail.com>
2022-05-09 21:19:14 -0500

    Modificacion de Home y carga del logo

--- allcomics-front/src/Paginas/Home.jsx
+++ allcomics-front/src/Paginas/Home.jsx
@@ -1,60 +1,75 @@
 import CarroCompra from "./CarroCompra";
-import { Carousel, Col, Row } from "react-bootstrap";
+import { Carousel, Col, Row, Button } from "react-bootstrap";
 import { Container, } from "react-bootstrap";
 import Carrusel_Layout1 from "../Assests/Carrusel_Layout1.png";
 import Layout1 from "../Assests/Layout1.png"
 
 function Home(props) {
   return (
-    <Container>
+
+    <>
       <Row>
-                <Col>
-                <img
-            className="d-block w-100"
-            src={Layout1}/>
-                </Col>
-                <Col>
-      <Carousel>
-        <Carousel.Item>
+        <Col >
           <img
             className="d-block w-100"
-            src={Carrusel_Layout1}
-            alt="Wellcome to ALLComics"
-          />
-          <Carousel.Caption>
-        
-          <p>Tienda de comics no autorizada</p>
-          </Carousel.Caption>
-        </Carousel.Item>
-        <Carousel.Item>
-          <img
-            className="d-block w-100"
-            src={Carrusel_Layout1}
-            alt="Second slide"
-          />
+            src={Layout1} />
+        </Col>
+        <Col>
+          <Carousel>
+            <Carousel.Item>
+              <img
+                className="d-block w-100"
 
-          <Carousel.Caption>
-            <h3>Coming Soon...</h3>
-            <p>Estaremos subiendo contenido proximamente.</p>
-          </Carousel.Caption>
-        </Carousel.Item>
-        <Carousel.Item>
-          <img
-            className="d-block w-100"
-            src={Carrusel_Layout1}
-            alt="Third slide"
-          />
+                src={Carrusel_Layout1}
+                alt="Wellcome to ALLComics"
+              />
+              <Carousel.Caption>
 
-          <Carousel.Caption>
-            <h3>Coming Soon...</h3>
-            <p>Estaremos subiendo contenido proximamente.</p>
-          </Carousel.Caption>
-        </Carousel.Item>
-      </Carousel>
-      </Col>
+                <p>Tienda de comics no autorizada</p>
+              </Carousel.Caption>
+            </Carousel.Item>
+            <Carousel.Item>
+              <img
+                className="d-block 	w-100"
+                src={Carrusel_Layout1}
+                alt="Second slide"
+              />
+
+              <Carousel.Caption>
+                <h3>Coming Soon...</h3>
+                <p>Estaremos subiendo contenido proximamente.</p>
+              </Carousel.Caption>
+            </Carousel.Item>
+            <Carousel.Item>
+              <img
+                className="d-block w-100"
+                src={Carrusel_Layout1}
+                alt="Third slide"
+              />
+
+              <Carousel.Caption>
+                <h3>Coming Soon...</h3>
+                <p>Estaremos subiendo contenido proximamente.</p>
+              </Carousel.Caption>
+            </Carousel.Item>
+          </Carousel>
+        </Col>
       </Row>
-      <CarroCompra/>
-    </Container>
+
+      <Row>
+        <h1 style={{ color: '#138CFF' }}>
+          Nuevos Lanzamientos
+          <Button
+            variant="outline-secondary">
+            Ver mas...
+          </Button>
+        </h1>
+      </Row>
+      <Row></Row>
+
+      <Container>
+      </Container>
+    </>
   );
 }
 
